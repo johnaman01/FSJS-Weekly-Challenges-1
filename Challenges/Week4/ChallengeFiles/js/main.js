0d12ff1604267383c5f04b020d753e404c69ba2f
John <johnaman01@gmail.com>
2016-09-26 19:25:59 -0400

    incomplete challenge 9_19_16

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -6,13 +6,35 @@ function openBlock (id) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
-    // TODO: remove the "active" class from all of the li elements inside the menu
-    // TODO: add the "active" class to the li element that contains the link that was clicked
+    // TODO: make clicking on menu buttons open blocks of content to the right 
+
 
 }
 
+// TODO: remove the "active" class from all of the li elements inside the menu
+// TODO: add the "active" class to the li element that contains the link that was clicked
+$('#menu li').removeClass('active'); 
+$('#menu li').click(function(){
+  $('#menu li').removeClass('active'); 
+  $(this).addClass('active');
+}); 
+
+
 // TODO: add the "hover" class to the menu items when you hover over them
+$('#menu a').hover(function(){
+  console.log("I'm here!");
+  $(this).addClass('hover'); 
+  }, 
+  function(){
+  $(this).removeClass('hover');
+});
+
+
+
+
+$().click(function(){
+  
+});
 
 
 // TODO: set up the tooltip plugin on all of the links in the menu
